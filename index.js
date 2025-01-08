64a72ba6cb261685f7bb52bcee836e1ec1900af8
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-01-08 11:59:39 +0200

    Refactor javascript code
    -add support for 'x' button to be used for multiplication sign
    -simply if else clauses by remocing unnecesary curly brackets
    -use functions to make code more reusable
    -add keyboard support by:
     *creating arrays to store the the event code of number, operator and action buttons
     *adding event listner for 'keydown'
     *using if, else clauses to run different functions depending on the event code
     *adding functions for adding operators, numbers to the display, clearing the display, backspace functionality, updating display with result of evaluation and adding decimal number support

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function operate(num1, operator, num2){
     {
         return subtract(num1, num2)
     }
-    else if(operator == 'X')
+    else if(operator == 'X' || operator == 'x')
     {
         return multiply(num1, num2)
     }
@@ -83,91 +83,21 @@ let pointCount = 0;
 function populate(element){
     element.addEventListener("click", function(){
         if(element == deleteBtn)
-        {
-            displayEL.value = ""
-            operatorIndex = 0
-            operatorCount = 0
-            pointCount = 0
-        }
+            clear()
         else if(element == backBtn)
-        {
-            let string = String(displayEL.value)
-            if(string != 'ERROR'){
-                string = string.slice(0, string.length - 1)
-                displayEL.value = string
-            }
-            else{
-                displayEL.value = ""
-            }
-
-        }
+            backspace()
         else if((element == plusBtn || 
                 element == minusBtn || 
                 element == timesBtn || 
-                element == divideBtn) && displayEL.value != 'ERROR')
-        {
-            operatorCount++
-            if(operatorCount > 1)
-            {
-                let string = String(displayEL.value)
-                let string1 = string.slice(0, operatorIndex-1)
-                let operator = string.slice(operatorIndex-1, operatorIndex)
-                let string2 = string.slice(operatorIndex, string.length)
-                number1 = operate(Number(string1), operator, Number(string2))
-                if(number1 % 1 != 0 && number1 != "ERROR")
-                {
-                    number1 = number1.toFixed(8)
-                    pointCount = 1;
-                }
-                else{
-                    number1
-                }
-                displayEL.value = number1
-                displayEL.value += element.innerText
-                operatorCount = 1
-                operatorIndex = displayEL.value.length
-            }
-            else{
-            displayEL.value += element.innerText
-            let string = String(displayEL.value)
-            operatorIndex = string.length
-            string = string.slice(0, string.length - 1)
-            number1 = Number(string)
-            }
-            pointCount = 0;
-        }
+                element == divideBtn) && 
+                displayEL.value != 'ERROR')
+            op(element.innerText);
         else if(element == equalsBtn)
-        {
-            if(operatorIndex != 0){
-                let string = String(displayEL.value)
-                let newString = string.slice(operatorIndex, string.length)
-                number2 = Number(newString)
-                let solution = operate(number1, string[operatorIndex-1],number2)
-                if(solution % 1 != 0 && solution != "ERROR")
-                {
-                        solution = solution.toFixed(8)
-                        pointCount = 1;
-                }
-                else{
-                    solution = solution
-                    pointCount = 0
-                }
-                displayEL.value = solution
-            }
-            operatorIndex = 0
-            operatorCount = 0
-        }
-        else if(element == pointBtn){
-            if (pointCount < 1){
-                displayEL.value += element.innerText
-                pointCount++
-            }
-        }
+            equals()
+        else if(element == pointBtn)
+            decimal(element.innerText)
         else
-        {
-            if(displayEL.value != 'ERROR')
-                displayEL.value += element.innerText
-        }
+            standard(element.innerText)
     })
 }
 
@@ -190,3 +120,121 @@ populate(equalsBtn)
 populate(deleteBtn)
 populate(backBtn)
 populate(pointBtn)
+
+let numbers = ["Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0"]
+let operators = ["KeyX", "Minus", "Slash", "NumpadAdd"]
+let actualizers = ["Backspace", "Equal", "Delete"]
+
+document.addEventListener('keydown', function(event){
+    console.log(event.code)
+    if(numbers.includes(event.code))
+        standard(event.key)
+    else if(operators.includes(event.code)){
+        if(event.code != "Slash")
+            op(event.key)
+        else
+            op(divideBtn.innerText)
+        
+    }
+    else if(actualizers.includes(event.code))
+    {
+        if(event.code == "Delete")
+            clear()
+        else if(event.code == "Backspace")
+            backspace()
+        else
+            equals()
+    }
+    else if(event.code == "Period")
+    {
+        decimal(event.key)
+    }
+} );
+
+function op(element){
+    operatorCount++
+            if(operatorCount > 1)
+            {
+                let string = String(displayEL.value)
+                let string1 = string.slice(0, operatorIndex-1)
+                let operator = string.slice(operatorIndex-1, operatorIndex)
+                let string2 = string.slice(operatorIndex, string.length)
+                number1 = operate(Number(string1), operator, Number(string2))
+                if(number1 % 1 != 0 && number1 != "ERROR")
+                {
+                    number1 = number1.toFixed(8)
+                    pointCount = 1;
+                }
+                else{
+                    number1
+                }
+                displayEL.value = number1
+                displayEL.value += element
+                operatorCount = 1
+                operatorIndex = displayEL.value.length
+            }
+            else{
+            displayEL.value += element
+            let string = String(displayEL.value)
+            operatorIndex = string.length
+            string = string.slice(0, string.length - 1)
+            number1 = Number(string)
+            }
+            pointCount = 0;
+}
+
+function standard(digit){
+    if(displayEL.value != 'ERROR')
+        displayEL.value += digit
+}
+
+function clear()
+{
+    displayEL.value = ""
+    operatorIndex = 0
+    operatorCount = 0
+    pointCount = 0
+}
+
+function backspace()
+{
+    let string = String(displayEL.value)
+    if(string != 'ERROR'){
+        string = string.slice(0, string.length - 1)
+        displayEL.value = string
+    }
+    else{
+       displayEL.value = ""
+    }
+}
+
+function equals()
+{
+    if(operatorIndex != 0){
+        let string = String(displayEL.value)
+        let newString = string.slice(operatorIndex, string.length)
+        let operator = string[operatorIndex-1]
+        number2 = Number(newString)
+        let solution = operate(number1, operator,number2)
+        if(solution % 1 != 0 && solution != "ERROR")
+        {
+                solution = solution.toFixed(8)
+                pointCount = 1;
+        }
+        else{
+            solution = solution
+            pointCount = 0
+        }
+        displayEL.value = solution
+    }
+    operatorIndex = 0
+    operatorCount = 0
+}
+
+function decimal(element)
+{
+    if (pointCount < 1){
+        displayEL.value += element
+        pointCount++
+    }
+}
